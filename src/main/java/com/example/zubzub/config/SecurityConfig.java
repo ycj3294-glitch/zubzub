ab407fee3788bc2cad41df75c4c193ca30a5bef3
yang <ycj3294@gmail.com>
2025-12-16 09:17:33 +0900

    통합 실행 확인

--- src/main/java/com/example/zubzub/config/SecurityConfig.java
+++ src/main/java/com/example/zubzub/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.zubzub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -43,5 +45,9 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
