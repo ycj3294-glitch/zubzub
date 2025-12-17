44d940ec39b547ca90be9d2add2c99bbc9a21a9a
hallispio <bureum66@gmail.com>
2025-12-17 09:17:10 +0900

    병합 한 커밋

--- src/main/java/com/example/zubzub/repository/AuctionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.zubzub.repository;
-
-import com.example.zubzub.entity.Auction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuctionRepository extends JpaRepository<Auction, Long> {
-}
